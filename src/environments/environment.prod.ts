275e1a7d5724aea9c4e7eaa45791ac0ce17bf328
AlexanderBuyanov1972 <AlexanderBuyanov1972@users.noreply.github.com>
2019-07-21 00:36:25 +0300

    2019/07/21

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true, firebaseConfig : {
+    apiKey: 'AIzaSyCqgSbsKEyDKIJ6HBSTSsJLkblovuxjXb0',
+    authDomain: 'rent-company-md-fb.firebaseapp.com',
+    databaseURL: 'https://rent-company-md-fb.firebaseio.com',
+    projectId: 'rent-company-md-fb',
+    storageBucket: 'rent-company-md-fb.appspot.com',
+    messagingSenderId: '801875531294',
+    appId: '1:801875531294:web:d6e0853c78af3934'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, firebaseConfig : {
+    apiKey: 'AIzaSyCqgSbsKEyDKIJ6HBSTSsJLkblovuxjXb0',
+    authDomain: 'rent-company-md-fb.firebaseapp.com',
+    databaseURL: 'https://rent-company-md-fb.firebaseio.com',
+    projectId: 'rent-company-md-fb',
+    storageBucket: 'rent-company-md-fb.appspot.com',
+    messagingSenderId: '801875531294',
+    appId: '1:801875531294:web:d6e0853c78af3934'
+  }
 };
 
 /*
